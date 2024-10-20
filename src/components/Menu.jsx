de67461681695261014cb962f357114133ba2f12
SadaaqueAli <sadaaqueali@gamil.com>
2024-10-21 00:11:29 +0500

    video-app files cretae

---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+flex:1;
+background-color: #202020;
+height:100vh;
+color: white;
+`
+const Wrapper = styled.div`
+padding: 18px 26px;
+`
+
+export default function Menu() {
+    return (
+        <Container>
+            <Wrapper>items</Wrapper>
+        </Container>
+    )
+}
